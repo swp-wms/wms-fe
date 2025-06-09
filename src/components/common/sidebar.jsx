628ae56a136d46cc0e0677e7813d13160f495075
Lilyhouser <houselily010@gmail.com>
2025-06-10 00:43:07 +0700

    add: set user

--- src/components/common/sidebar.jsx
+++ src/components/common/sidebar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBars,
@@ -12,6 +12,7 @@ import {
   faCircleUser,
   faPowerOff,
 } from "@fortawesome/free-solid-svg-icons";
+import { handleLogout } from "../../backendCalls/user";
 
 const sideElement = [
   {
@@ -19,50 +20,57 @@ const sideElement = [
     icon: faHouse,
     path: "/tong-quan-kho",
     name: "Tổng quan kho",
+    allowed: [3, 4]
   },
   {
     id: 2,
     icon: faInbox,
     path: "/nhap-hang",
     name: "Nhập hàng",
+    allowed: [3, 4]
   },
   {
     id: 3,
     icon: faRightFromBracket,
     path: "/xuat-hang",
     name: "Xuất hàng",
+    allowed: [3, 4]
   },
   {
     id: 4,
     icon: faChartSimple,
     path: "/thong-ke-kho",
     name: "Thống kê kho",
+    allowed: [3]
   },
   {
     id: 5,
     icon: faTruckFast,
     path: "/ke-hoach-van-chuyen",
     name: "Kế hoạch vận chuyển",
+    allowed: [3, 4]
   },
   {
     id: 6,
     icon: faFile,
     path: "/cac-loai-don-tu-khac",
     name: "Các loại đơn từ khác",
-  },
+    allowed: [3, 4]
+  }, {
+    id: 7,
+    icon: faFile,
+    path: "/danh-sach-nguoi-dung",
+    name: "Danh sách người dùng",
+    allowed: [1]
+  }
 ];
 
-const role = [
-  {
-    id: 1,
-    name: "Salesman",
-  },
-  {
-    id: 2,
-    name: "Warehouse Keeper",
-  },
-];
-const Sidebar = () => {
+const Sidebar = ({ user }) => {
+  const navigate = useNavigate();
+  const logout = async () => {
+    await handleLogout();
+    navigate('/dang-nhap');
+  }
   return (
     <div className="flex flex-col h-screen w-[300px] bg-white px-6 shadow-lg justify-between">
       <div>
@@ -72,15 +80,20 @@ const Sidebar = () => {
         </div>
         <hr className="opacity-10" />
         <ul className="flex flex-col gap-2 mt-8">
-          {sideElement.map((item) => (
-            <li
-              key={item.id}
-              className=" text-gray-600 py-2 hover-btn"
-            >
-              <FontAwesomeIcon icon={item.icon} className="w-1/5" />
-              <Link to={item.path}>{item.name}</Link>
-            </li>
-          ))}
+          {sideElement.map((item) => {
+            if (item.allowed.includes(user.roleid)) {
+
+              return (
+                <li
+                  key={item.id}
+                  className=" text-gray-600 py-2 hover-btn"
+                >
+                  <FontAwesomeIcon icon={item.icon} className="w-1/5" />
+                  <Link to={item.path}>{item.name}</Link>
+                </li>
+              )
+            }
+          })}
         </ul>
       </div>
 
@@ -88,11 +101,11 @@ const Sidebar = () => {
         <div className="flex flex-row rounded-full bg-white shadow-btn p-2 w-fit justify-between items-center gap-2">
           <FontAwesomeIcon icon={faCircleUser} size="xl" />
           <span className="px-3 font-medium">
-            {role.filter((item) => item.id === 1)[0].name}
+            {user.role.rolename}
           </span>
         </div>
         <div className="rounded-full bg-red-500 size-8 flex items-center justify-center shadow-btn">
-          <FontAwesomeIcon icon={faPowerOff} color="white" />
+          <FontAwesomeIcon icon={faPowerOff} onClick={logout} title="Logout" className="hover:cursor-pointer" color="white" />
         </div>
       </div>
     </div>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,8 @@
 import { useState } from "react"
 import { handleLogin } from "../backendCalls/user";
-import { useNavigate } from "react-router-dom";
 
 
 const Login = () => {
-  const navigate = useNavigate();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [show, setShow] = useState(false);
@@ -13,12 +11,11 @@ const Login = () => {
   const login = async (e) => {
     e.preventDefault();
     const response = await handleLogin(username, password);
-    console.log('data',response);
-    if(response.status !== 200) {      
+    if (response.status !== 200) {
       setError(response.response.data.message);
     } else {
       setError('');
-      navigate('/');
+      window.location.href = '/';
     }
   }
 
@@ -30,8 +27,8 @@ const Login = () => {
       <div className="fixed top-0 bottom-0 left-0 right-0 flex items-center justify-center">
         <form className="bg-white p-8 rounded-md w-[360px] flex flex-col items-center">
           <img className="mb-4" src="/logo.png" width={'160px'} alt="logo" />
-          
-          {error && <p style={{color: 'red', fontWeight: 'normal'}}>{error}</p>}
+
+          {error && <p style={{ color: 'red', fontWeight: 'normal' }}>{error}</p>}
 
           <input className="login-input bg-[#fae5e2] placeholder:text-[#e18479]"
             type="text"
