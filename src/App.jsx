ce66e42c1ce4593e6ad6bb8416f32e347425a62d
ngocmaii00 <mailyngoc2301@gmail.com>
2025-06-10 07:51:17 +0700

    fix:header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import ForgotPassword from './pages/ForgotPassword';
 
-
 function App() {
+  const [user, setUser] = useState();
+
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home setUser={setUser} user={user} />} />
         <Route path="/dang-nhap" element={<Login />} />
         <Route path='/quen-mat-khau' element={<ForgotPassword />} />
         <Route path="/tong-quan-kho" element={<Home />} />
