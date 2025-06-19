f9ba5016b31ae5b106fbd24f3a9d6d8009dd8c99
TheDarkDuck <thefastestmanal@gmail.com>
2025-06-19 21:39:12 +0700

    admin page v1

--- src/components/admin/AccountManage.jsx
+++ src/components/admin/AccountManage.jsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { useEffect, useState } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons"
