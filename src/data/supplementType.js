1c908fda587123d23f146a2fc0d6c0149f11e529
Tran Dang Viet Huy <viethuy2904@gmail.com>
2025-07-02 00:34:56 +0700

    slightly change on supplement order layout

---
+++ src/data/supplementType.js
@@ -0,0 +1,13 @@
+const supplementType = [
+    {
+        "code": "I",
+        "name": "Đơn nhập bù"
+    },
+    {
+        "code": "E",
+        "name": "Đơn xuất bù"
+    }
+
+]
+
+export default supplementType
