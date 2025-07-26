cb0a49a0705f564641360c10b5aac36daaf22435
Lilyhouser <houselily010@gmail.com>
2025-07-26 13:41:47 +0700

    fix: date of export/import note. fix: layer of calendar fix: hidden complete order

--- src/components/delivery/OrderList.jsx
+++ src/components/delivery/OrderList.jsx
@@ -4,16 +4,18 @@ import Order from "./Order"
 const OrderList = ({ orders, setCurrentOrder, user, setDeliverySchedule, setCurrentDelivery, setCurrentDeliveryDetail }) => {
     return (
         <ul className="text-[15px] h-[90%] overflow-y-scroll p-[4px]">
-            {orders.map((order) => (
-                <Order key={order.orderid}
-                    setCurrentDelivery={setCurrentDelivery}
-                    setCurrentDeliveryDetail={setCurrentDeliveryDetail}
-                    setDeliverySchedule={setDeliverySchedule}
-                    order={order}
-                    setCurrentOrder={setCurrentOrder}
-                    user={user}
-                />
-            ))}
+            {orders.map((order) => {
+                if (order.status !== 'XONG') {
+                    return <Order key={order.orderid}
+                        setCurrentDelivery={setCurrentDelivery}
+                        setCurrentDeliveryDetail={setCurrentDeliveryDetail}
+                        setDeliverySchedule={setDeliverySchedule}
+                        order={order}
+                        setCurrentOrder={setCurrentOrder}
+                        user={user}
+                    />
+                }
+            })}
         </ul>
     )
 }
